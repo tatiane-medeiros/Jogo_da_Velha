60ad861f8bf0bbbca43dd579d213dd534221e84a
tatiane-medeiros <mtatianemedeiros@gmail.com>
2015-12-18 09:42:09 -0300

    Final

--- grid.h
+++ grid.h
@@ -13,12 +13,18 @@ public:
     explicit Grid(QObject *parent = 0);
     ~Grid();
     const QVector<int> data();
+    //linha onde está a sequencia que ganha o jogo
     int linePosition;
     Q_INVOKABLE int line();
+    //esvazia o array
     Q_INVOKABLE void clear();
+    //altera um valor no array
     Q_INVOKABLE void setValue(int pos, int value);
+    //verifica se todas as casas ja foram preenchidas
     Q_INVOKABLE bool isFull();
+    //verifica se há vencedor
     Q_INVOKABLE int win();
+    //retorna o valor em uma posição do array
     Q_INVOKABLE int atPos(int index);   
 
 signals:
